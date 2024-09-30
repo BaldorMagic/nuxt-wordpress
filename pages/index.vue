73ebfb98dc7a35b6672d11f1ff451173c75c2f5b
Baldor <james@baldor.dev>
2024-09-30 18:28:55 +0200

    header component created/styled

--- pages/index.vue
+++ pages/index.vue
@@ -1,2 +1,9 @@
-<script setup></script>
-<template></template>
+<script setup lang="ts">
+const route = useRoute()
+</script>
+<template>
+    <div>
+        <TheHeader />
+        <div class="grid gap-8 grid-cols-1 lg:grid-cols-3 p-6"></div>
+    </div>
+</template>
