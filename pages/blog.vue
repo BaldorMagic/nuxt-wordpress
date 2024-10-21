9ac992b48ced0ab9d76df075b67eedb799ba5646
Baldor <james@baldor.dev>
2024-10-21 21:03:52 +0200

    blog posts added

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>blog</h1>
+    <h1>Blog</h1>
+    <article v-for="post in posts" :key="post.id">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.body.slice(0,1000) }}</p>
+        <p>
+            <NuxtLink :to="`/blog/${post.id}`"">Lire la suite</NuxtLink>
+        </p>
+    </article>
 </template>
+<script setup>
+    const {data: posts} = useFetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+</script>
